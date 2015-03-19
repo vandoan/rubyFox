b9854909701c057877af7547871d8db1719b020e
Van Doan <vdoan09@gmail.com>
2015-03-18 23:55:19 -0400

    update sandbox e

--- sand3.rb
+++ sand3.rb
@@ -1,4 +1,17 @@
-print "what's your first name?" 
-first_name = gets.chomp
 
-puts first_name
+
+print "what's your first name?" 
+    first_name = gets.chomp
+print "what's your last name?" 
+    last_name = gets.chomp
+print "What city are you from from?" 
+    city = gets.chomp
+print "What state are you from?" 
+    state = gets.chomp
+  
+  
+  print first_name
+  print last_name
+  
+  print city
+  print state.upcase

dccb8482a057dfab8acf64308b639867aba40978
Van Doan <vdoan09@gmail.com>
2015-03-19 23:45:08 -0400

    gsub and others

---
+++ negPosNumber.rb
@@ -0,0 +1,11 @@
+print "Integer please: "
+user_num = 4
+
+
+if user_num < 0
+  puts "You picked a negative integer!"
+elsif user_num > 0
+  puts "You picked a positive integer!"
+else
+  puts "You picked zero!"
+end

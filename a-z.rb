c0e3d04727cfd3724f7985b2517034a2235c3c07
Van Doan <vdoan09@gmail.com>
2015-04-05 23:00:15 -0400

    secret message project

---
+++ a-z.rb
@@ -0,0 +1,16 @@
+lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","v","w","x","y","z"]
+# puts lower.length
+# for i in 0..lower.length
+	# print lower[i]
+
+
+puts $var3
+
+def displace(string) 
+	string.chars.to_a
+	puts string[0]
+	# convert letter to number]
+end
+
+
+ puts displace("dog")

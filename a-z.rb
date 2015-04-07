897c8a8e5218d4f7edccae886b2571dcff2321da
Van Doan <vdoan09@gmail.com>
2015-04-06 22:23:59 -0400

    caesar cypher 2

--- a-z.rb
+++ a-z.rb
@@ -1,16 +1,19 @@
 lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","v","w","x","y","z"]
-# puts lower.length
-# for i in 0..lower.length
-	# print lower[i]
+numero = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27 ]
 
 
-puts $var3
-
-def displace(string) 
-	string.chars.to_a
-	puts string[0]
-	# convert letter to number]
-end
+ascii = "test".chars.map(&:ord)
+# chars - split string into array
+# ord - turns the character to its ascii equilent
+# map - transform all the elements of an array 
+# returns 116 101 115 116
 
 
- puts displace("dog")
+# takes the number and add 5 to each one
+shifted = ascii.map { |c| c + 5 }
+
+# transform to letter and rejoin
+shifted.map { |c| c.chr }.join
+
+
+puts shifted

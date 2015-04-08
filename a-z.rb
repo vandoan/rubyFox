04b1ff8840e75c5202b4083ddbb159bc691bd916
Van Doan <vdoan09@gmail.com>
2015-04-07 23:40:10 -0400

    ruby sandbox

--- a-z.rb
+++ a-z.rb
@@ -1,3 +1,5 @@
+# http://www.blackbytes.info/2015/03/caesar-cipher-in-ruby/
+
 lower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","w","v","w","x","y","z"]
 numero = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27 ]
 
@@ -12,8 +14,8 @@ ascii = "test".chars.map(&:ord)
 # takes the number and add 5 to each one
 shifted = ascii.map { |c| c + 5 }
 
-# transform to letter and rejoin
+# use char to transform number to letter and then rejoin
 shifted.map { |c| c.chr }.join
 
 
-puts shifted
+puts shifted	
---
+++ shuffle.rb
@@ -0,0 +1,7 @@
+puts [5, 15, 20, 25, 30, 35, 40].shuffle.first
+
+print   [5, 15, 20, 25, 30, 35, 40].shuffle
+
+puts [5, 15, 20, 25, 30, 35, 40].sample
+
+puts ('a'..'z').to_a.sample
